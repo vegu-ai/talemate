a9db7b7d3bc3829e8b6e2ebfba70995c27b72edb
FinalWombat <finalwombat@gmail.com>
2023-11-01 03:07:00 +0200

    token counts for openai client

--- src/talemate/client/openai.py
+++ src/talemate/client/openai.py
@@ -9,12 +9,63 @@ from talemate.emit import emit
 from talemate.config import load_config
 import talemate.client.system_prompts as system_prompts
 import structlog
+import tiktoken
 
 __all__ = [
     "OpenAIClient",
 ]
 
 log = structlog.get_logger("talemate")
+
+def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613"):
+    """Return the number of tokens used by a list of messages."""
+    try:
+        encoding = tiktoken.encoding_for_model(model)
+    except KeyError:
+        print("Warning: model not found. Using cl100k_base encoding.")
+        encoding = tiktoken.get_encoding("cl100k_base")
+    if model in {
+        "gpt-3.5-turbo-0613",
+        "gpt-3.5-turbo-16k-0613",
+        "gpt-4-0314",
+        "gpt-4-32k-0314",
+        "gpt-4-0613",
+        "gpt-4-32k-0613",
+    }:
+        tokens_per_message = 3
+        tokens_per_name = 1
+    elif model == "gpt-3.5-turbo-0301":
+        tokens_per_message = (
+            4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
+        )
+        tokens_per_name = -1  # if there's a name, the role is omitted
+    elif "gpt-3.5-turbo" in model:
+        print(
+            "Warning: gpt-3.5-turbo may update over time. Returning num tokens assuming gpt-3.5-turbo-0613."
+        )
+        return num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613")
+    elif "gpt-4" in model:
+        print(
+            "Warning: gpt-4 may update over time. Returning num tokens assuming gpt-4-0613."
+        )
+        return num_tokens_from_messages(messages, model="gpt-4-0613")
+    else:
+        raise NotImplementedError(
+            f"""num_tokens_from_messages() is not implemented for model {model}. See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens."""
+        )
+    num_tokens = 0
+    for message in messages:
+        num_tokens += tokens_per_message
+        for key, value in message.items():
+            if value is None:
+                continue
+            if isinstance(value, dict):
+                value = json.dumps(value)
+            num_tokens += len(encoding.encode(value))
+            if key == "name":
+                num_tokens += tokens_per_name
+    num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
+    return num_tokens
 
 @register()
 class OpenAIClient:
@@ -158,8 +209,8 @@ class OpenAIClient:
             "prompt": prompt,
             "response": response,
             # TODO use tiktoken
-            "prompt_tokens": "?",
-            "response_tokens": "?",
+            "prompt_tokens": num_tokens_from_messages([sys_message, human_message], model=self.model_name),
+            "response_tokens": num_tokens_from_messages([{"role": "assistant", "content": response}], model=self.model_name),
         })
 
         self.emit_status(processing=False)

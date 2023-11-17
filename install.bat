ddcd442821ef70d98dd6521febb27c494a116b6f
FinalWombat <finalwombat@gmail.com>
2023-11-17 20:55:13 +0200

    fix windows install script

--- install.bat
+++ install.bat
@@ -6,11 +6,11 @@ python -m venv talemate_env
 REM activate the virtual environment
 call talemate_env\Scripts\activate
 
-REM activate the virtual environment
-call talemate_env\Scripts\activate
-
 REM install poetry
 python -m pip install "poetry==1.7.1" "rapidfuzz>=3" -U
+
+REM use poetry to install dependencies
+python -m poetry install
 
 REM copy config.example.yaml to config.yaml only if config.yaml doesn't exist
 IF NOT EXIST config.yaml copy config.example.yaml config.yaml

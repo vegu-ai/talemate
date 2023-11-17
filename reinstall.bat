556fc0a551191c1d6fad7d166b97053ddb3013c2
FinalWombat <finalwombat@gmail.com>
2023-11-17 20:26:17 +0200

    switch back to poetry for windows as well

--- reinstall.bat
+++ reinstall.bat
@@ -8,8 +8,11 @@ python -m venv talemate_env
 REM activate the virtual environment
 call talemate_env\Scripts\activate
 
-REM install dependencies from requirements.txt
-python -m pip install -r requirements.txt
+REM install poetry
+python -m pip install "poetry==1.7.1" "rapidfuzz>=3" -U
+
+REM use poetry to install dependencies
+python -m poetry install
 
 echo Virtual environment re-created.
 pause

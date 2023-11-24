c08b654b8ef13cd384c995d7044e56390301cebe
FinalWombat <finalwombat@gmail.com>
2023-11-24 03:46:27 +0200

    editor fix narrator messages

--- src/talemate/agents/editor.py
+++ src/talemate/agents/editor.py
@@ -10,7 +10,7 @@ import talemate.emit.async_signals
 from talemate.prompts import Prompt
 from talemate.scene_message import DirectorMessage, TimePassageMessage
 
-from .base import Agent, set_processing, AgentAction
+from .base import Agent, set_processing, AgentAction, AgentActionConfig
 from .registry import register
 
 import structlog
@@ -21,6 +21,7 @@ import re
 if TYPE_CHECKING:
     from talemate.tale_mate import Actor, Character, Scene
     from talemate.agents.conversation import ConversationAgentEmission
+    from talemate.agents.narrator import NarratorAgentEmission
 
 log = structlog.get_logger("talemate.agents.editor")
 
@@ -40,7 +41,9 @@ class EditorAgent(Agent):
         self.is_enabled = True
         self.actions = {
             "edit_dialogue": AgentAction(enabled=False, label="Edit dialogue", description="Will attempt to improve the quality of dialogue based on the character and scene. Runs automatically after each AI dialogue."),
-            "fix_exposition": AgentAction(enabled=True, label="Fix exposition", description="Will attempt to fix exposition and emotes, making sure they are displayed in italics. Runs automatically after each AI dialogue."),
+            "fix_exposition": AgentAction(enabled=True, label="Fix exposition", description="Will attempt to fix exposition and emotes, making sure they are displayed in italics. Runs automatically after each AI dialogue.", config={
+                "narrator": AgentActionConfig(type="bool", label="Fix narrator messages", description="Will attempt to fix exposition issues in narrator messages", value=True),
+            }),
             "add_detail": AgentAction(enabled=False, label="Add detail", description="Will attempt to add extra detail and exposition to the dialogue. Runs automatically after each AI dialogue.")
         }
         
@@ -59,6 +62,7 @@ class EditorAgent(Agent):
     def connect(self, scene):
         super().connect(scene)
         talemate.emit.async_signals.get("agent.conversation.generated").connect(self.on_conversation_generated)
+        talemate.emit.async_signals.get("agent.narrator.generated").connect(self.on_narrator_generated)
         
     async def on_conversation_generated(self, emission:ConversationAgentEmission):
         """
@@ -89,6 +93,24 @@ class EditorAgent(Agent):
                 emission.character
             )
             
+            edited.append(edit)
+            
+        emission.generation = edited
+        
+    async def on_narrator_generated(self, emission:NarratorAgentEmission):
+        """
+        Called when a narrator message is generated
+        """
+        
+        if not self.enabled:
+            return
+        
+        log.info("editing narrator", emission=emission)
+        
+        edited = []
+        
+        for text in emission.generation:
+            edit = await self.fix_exposition_on_narrator(text)
             edited.append(edit)
             
         emission.generation = edited
@@ -127,17 +149,36 @@ class EditorAgent(Agent):
         if not self.actions["fix_exposition"].enabled:
             return content
         
-        #response = await Prompt.request("editor.fix-exposition", self.client, "edit_fix_exposition", vars={
-        #    "content": content,
-        #    "character": character,
-        #    "scene": self.scene,
-        #    "max_length": self.client.max_token_length
-        #})
+        if not character.is_player:
+            if '"' not in content and '*' not in content:
+                content = util.strip_partial_sentences(content)
+                character_prefix = f"{character.name}: "
+                message = content.split(character_prefix)[1]
+                content = f"{character_prefix}*{message.strip('*')}*"
+                return content
         
         content = util.clean_dialogue(content, main_name=character.name)        
         content = util.strip_partial_sentences(content)
         content = util.ensure_dialog_format(content, talking_character=character.name)
         
+        return content
+    
+    @set_processing
+    async def fix_exposition_on_narrator(self, content:str):
+        
+        if not self.actions["fix_exposition"].enabled:
+            return content
+        
+        if not self.actions["fix_exposition"].config["narrator"].value:
+            return content
+        
+        content = util.strip_partial_sentences(content)
+        
+        if '"' not in content:
+            content = f"*{content.strip('*')}*"
+        else:
+            content = util.ensure_dialog_format(content)
+               
         return content
     
     @set_processing

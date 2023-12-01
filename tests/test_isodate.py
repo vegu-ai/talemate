07b1d34bd3736e9c300246ba9d9b1ff3dc0f2dc5
vegu-ai-tools <152010387+vegu-ai-tools@users.noreply.github.com>
2023-12-01 03:29:47 +0200

    to pytest

--- tests/test_isodate.py
+++ tests/test_isodate.py
@@ -10,26 +10,29 @@ from talemate.util import (
     isodate
 )
 
-date1 = "P11MT15M"
-date2 = "PT1S"
 
-duration1= parse_duration_to_isodate_duration(date1)
-assert duration1.months == 11
-assert duration1.tdelta.seconds == 900
+def test_isodate_utils():
 
-duration2 = parse_duration_to_isodate_duration(date2)
-assert duration2.seconds == 1
+    date1 = "P11MT15M"
+    date2 = "PT1S"
 
-timedelta1 = duration_to_timedelta(duration1)
-assert timedelta1.seconds == 900
-assert timedelta1.days == 11*30, timedelta1.days
+    duration1= parse_duration_to_isodate_duration(date1)
+    assert duration1.months == 11
+    assert duration1.tdelta.seconds == 900
 
-timedelta2 = duration_to_timedelta(duration2)
-assert timedelta2.seconds == 1
+    duration2 = parse_duration_to_isodate_duration(date2)
+    assert duration2.seconds == 1
 
-parsed = parse_duration_to_isodate_duration("P11MT14M59S")
-assert iso8601_diff(date1, date2) == parsed, parsed
+    timedelta1 = duration_to_timedelta(duration1)
+    assert timedelta1.seconds == 900
+    assert timedelta1.days == 11*30, timedelta1.days
 
-assert iso8601_duration_to_human(date1) == "11 Months and 15 Minutes ago", iso8601_duration_to_human(date1)
-assert iso8601_duration_to_human(date2) == "1 Second ago",  iso8601_duration_to_human(date2)
-assert iso8601_duration_to_human(iso8601_diff(date1, date2)) == "11 Months, 14 Minutes and 59 Seconds ago", iso8601_duration_to_human(iso8601_diff(date1, date2))
+    timedelta2 = duration_to_timedelta(duration2)
+    assert timedelta2.seconds == 1
+
+    parsed = parse_duration_to_isodate_duration("P11MT14M59S")
+    assert iso8601_diff(date1, date2) == parsed, parsed
+
+    assert iso8601_duration_to_human(date1) == "11 Months and 15 Minutes ago", iso8601_duration_to_human(date1)
+    assert iso8601_duration_to_human(date2) == "1 Second ago",  iso8601_duration_to_human(date2)
+    assert iso8601_duration_to_human(iso8601_diff(date1, date2)) == "11 Months, 14 Minutes and 59 Seconds ago", iso8601_duration_to_human(iso8601_diff(date1, date2))

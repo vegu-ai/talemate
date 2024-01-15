f4215b4cb724a861e49d5ede466748e819917ff5
vegu-ai-tools <152010387+vegu-ai-tools@users.noreply.github.com>
2024-01-15 11:16:18 +0200

    template question / attribute cannot be empty

--- talemate_frontend/src/components/WorldStateManagerTemplates.vue
+++ talemate_frontend/src/components/WorldStateManagerTemplates.vue
@@ -49,85 +49,89 @@
                             {{ template.name  }}
                         </v-card-title>
                         <v-card-text>
-                            <v-row>
-                                <v-col cols="6">
-                                    <v-text-field 
-                                    v-model="template.query" 
-                                    label="Question or attribute name" 
-                                    required
-                                    hint="Available template variables: {character_name}, {player_name}" 
-                                    :color="dirty ? 'info' : ''"
-                                    @update:model-value="queueSaveTemplate">
-                                    </v-text-field>
-                                </v-col>
-                                <v-col cols="6">
-                                    <v-text-field v-model="template.description" 
-                                    hint="A short description of what this state is for."
-                                    :color="dirty ? 'info' : ''"
-                                    @update:model-value="queueSaveTemplate"
-                                    label="Description"></v-text-field>
-                                </v-col>
-                            </v-row>
-
-                            <v-row>
-                                <v-col cols="6">
-                                    <v-select 
-                                    v-model="template.insert" 
-                                    :items="insertionModes"
-                                    :color="dirty ? 'info' : ''"
-                                    @update:model-value="queueSaveTemplate"
-                                    label="Context Attachment Method">
-                                    </v-select>
-                                </v-col>
-                                <v-col cols="6">
-                                    <v-text-field v-model="template.interval" type="number" min="1" max="100"
-                                        label="Update every N turns"></v-text-field>
-                                </v-col>
-                            </v-row>
-
-                            <v-row>
-                                <v-col cols="12">
-                                    <v-textarea 
-                                        v-model="template.instructions"
-                                        label="Additional instructions to the AI for generating this state."
+                            <v-form ref="form" v-model="formValid">
+                                <v-row>
+                                    <v-col cols="6">
+                                        <v-text-field 
+                                        v-model="template.query" 
+                                        label="Question or attribute name" 
+                                        :rules="[v => !!v || 'Query is required']"
+                                        required
                                         hint="Available template variables: {character_name}, {player_name}" 
                                         :color="dirty ? 'info' : ''"
-                                        @update:model-value="queueSaveTemplate"
-                                        auto-grow
-                                        rows="3">
-                                    </v-textarea>
-                                </v-col>
-                            </v-row>
-
-                            <!-- three cols for the checkboxes -->
-
-                            <v-row>
-                                <v-col cols="4">
-                                    <v-select v-model="template.state_type"
-                                        :items="stateTypes"
+                                        @update:model-value="queueSaveTemplate">
+                                        </v-text-field>
+                                    </v-col>
+                                    <v-col cols="6">
+                                        <v-text-field v-model="template.description" 
+                                        hint="A short description of what this state is for."
                                         :color="dirty ? 'info' : ''"
                                         @update:model-value="queueSaveTemplate"
-                                        hint="What type of character / object is this state for?"
-                                        label="State type">
-                                    </v-select>
-                                </v-col>
-                                <v-col cols="4">
-                                    <v-checkbox 
-                                    v-model="template.auto_create" 
-                                    label="Automatically create" 
-                                    @update:model-value="queueSaveTemplate"
-                                    messages="Automatically create instances of this template for new games / characters."></v-checkbox>
-
-                                </v-col>
-                                <v-col cols="4">
-                                    <v-checkbox 
-                                    v-model="template.favorite" 
-                                    label="Favorite" 
-                                    @update:model-value="queueSaveTemplate"
-                                    messages="Favorited templates will be available for quick setup."></v-checkbox>
-
-                                </v-col>
-                            </v-row>
+                                        label="Description"></v-text-field>
+                                    </v-col>
+                                </v-row>
+    
+                                <v-row>
+                                    <v-col cols="6">
+                                        <v-select 
+                                        v-model="template.insert" 
+                                        :items="insertionModes"
+                                        :color="dirty ? 'info' : ''"
+                                        @update:model-value="queueSaveTemplate"
+                                        label="Context Attachment Method">
+                                        </v-select>
+                                    </v-col>
+                                    <v-col cols="6">
+                                        <v-text-field v-model="template.interval" type="number" min="1" max="100"
+                                            label="Update every N turns"></v-text-field>
+                                    </v-col>
+                                </v-row>
+    
+                                <v-row>
+                                    <v-col cols="12">
+                                        <v-textarea 
+                                            v-model="template.instructions"
+                                            label="Additional instructions to the AI for generating this state."
+                                            hint="Available template variables: {character_name}, {player_name}" 
+                                            :color="dirty ? 'info' : ''"
+                                            @update:model-value="queueSaveTemplate"
+                                            auto-grow
+                                            rows="3">
+                                        </v-textarea>
+                                    </v-col>
+                                </v-row>
+    
+                                <!-- three cols for the checkboxes -->
+    
+                                <v-row>
+                                    <v-col cols="4">
+                                        <v-select v-model="template.state_type"
+                                            :items="stateTypes"
+                                            :color="dirty ? 'info' : ''"
+                                            @update:model-value="queueSaveTemplate"
+                                            hint="What type of character / object is this state for?"
+                                            label="State type">
+                                        </v-select>
+                                    </v-col>
+                                    <v-col cols="4">
+                                        <v-checkbox 
+                                        v-model="template.auto_create" 
+                                        label="Automatically create" 
+                                        @update:model-value="queueSaveTemplate"
+                                        messages="Automatically create instances of this template for new games / characters."></v-checkbox>
+    
+                                    </v-col>
+                                    <v-col cols="4">
+                                        <v-checkbox 
+                                        v-model="template.favorite" 
+                                        label="Favorite" 
+                                        @update:model-value="queueSaveTemplate"
+                                        messages="Favorited templates will be available for quick setup."></v-checkbox>
+    
+                                    </v-col>
+                                </v-row>
+                            </v-form>
+                            
 
                         </v-card-text>
                         <v-card-actions>
@@ -165,6 +169,7 @@ export default {
             selectedTemplateIndex: null,
             saveTemplateTimeout: null,
             templateDeleteConfirm: false,
+            formValid: false,
             dirty: false,
             stateTypes: [
                 { "title": 'All characters', "value": 'character' },
@@ -268,6 +273,17 @@ export default {
 
         // requests 
         saveTemplate() {
+
+            if(!this.template) {
+                return;
+            }
+
+            this.$refs.form.validate()
+
+            if(!this.formValid) {
+                return;
+            }
+
             this.getWebsocket().send(JSON.stringify({
                 type: 'world_state_manager',
                 action: 'save_template',

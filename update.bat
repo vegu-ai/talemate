e6be1d0ca67c81a497ff2fb09d2a74a1c9377948
vegu-ai-tools <152010387+vegu-ai-tools@users.noreply.github.com>
2024-01-18 11:19:36 +0200

    init git repo if not exist

--- update.bat
+++ update.bat
@@ -1,5 +1,14 @@
 @echo off
 
+REM check if git repository is initialized and initialize if not
+if not exist .git (
+git init
+git remote add origin https://github.com/vegu-ai/talemate
+)
+
+REM pull the latest changes from git repository
+git pull origin master
+
 REM activate the virtual environment
 call talemate_env\Scripts\activate
 

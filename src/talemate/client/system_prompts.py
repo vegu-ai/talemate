eb251d6e3739b355b2f5b530fa959fbff84c0634
veguAI <152010387+vegu-ai-tools@users.noreply.github.com>
2024-02-01 12:15:30 +0200

    fix gpt-4 censorship triggered by system message (#74)

--- src/talemate/client/system_prompts.py
+++ src/talemate/client/system_prompts.py
@@ -19,3 +19,23 @@ EDITOR = str(Prompt.get("editor.system"))
 WORLD_STATE = str(Prompt.get("world_state.system-analyst"))
 
 SUMMARIZE = str(Prompt.get("summarizer.system"))
+
+# CAREBEAR PROMPTS
+
+ROLEPLAY_NO_DECENSOR = str(Prompt.get("conversation.system-no-decensor"))
+
+NARRATOR_NO_DECENSOR = str(Prompt.get("narrator.system-no-decensor"))
+
+CREATOR_NO_DECENSOR = str(Prompt.get("creator.system-no-decensor"))
+
+DIRECTOR_NO_DECENSOR = str(Prompt.get("director.system-no-decensor"))
+
+ANALYST_NO_DECENSOR = str(Prompt.get("world_state.system-analyst-no-decensor"))
+
+ANALYST_FREEFORM_NO_DECENSOR = str(Prompt.get("world_state.system-analyst-freeform-no-decensor"))
+
+EDITOR_NO_DECENSOR = str(Prompt.get("editor.system-no-decensor"))
+
+WORLD_STATE_NO_DECENSOR = str(Prompt.get("world_state.system-analyst-no-decensor"))
+
+SUMMARIZE_NO_DECENSOR = str(Prompt.get("summarizer.system-no-decensor"))

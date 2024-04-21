0d964302a2e8517f6e023d8a7c1cd6daaaad5758
vegu-ai-tools <152010387+vegu-ai-tools@users.noreply.github.com>
2024-04-21 19:39:06 +0300

    screenplay dialogue generation tweaks

--- src/talemate/agents/conversation.py
+++ src/talemate/agents/conversation.py
@@ -582,6 +582,8 @@ class ConversationAgent(Agent):
         result = result.replace("[", "*").replace("]", "*")
         result = result.replace("(", "*").replace(")", "*")
         result = result.replace("**", "*")
+        
+        result = util.handle_endofline_special_delimiter(result)
 
         return result
 
@@ -664,6 +666,8 @@ class ConversationAgent(Agent):
 
         total_result = total_result.split("#")[0].strip()
         
+        total_result = util.handle_endofline_special_delimiter(total_result)        
+
         if total_result.startswith(":\n"):
             total_result = total_result[2:]
 
---
+++ src/talemate/util/dialogue.py
@@ -0,0 +1,14 @@
+__all__ = [
+    "handle_endofline_special_delimiter"
+]
+
+def handle_endofline_special_delimiter(content:str) -> str:
+    # -- endofline -- is a custom delimter that can exist 0 to n times
+    # it should split total_result on the last one, take the left side
+    # then remove all remaining -- endofline -- from the left side
+    # then remove all leading and trailing whitespace
+    
+    content = content.rsplit("-- endofline --", 1)[0]
+    content = content.replace("-- endofline --", "")
+    content = content.strip()
+    return content

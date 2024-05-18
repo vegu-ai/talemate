29208e0403d2f8d895eba83dc72ce1b491ca8061
Stefan Pratter <vegu@vegui.org>
2024-05-18 14:50:30 +0300

    fix const var issue when ALLOWED_HOSTS is anything but `all`

--- talemate_frontend/vue.config.js
+++ talemate_frontend/vue.config.js
@@ -1,12 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 
-const ALLOWED_HOSTS = process.env.ALLOWED_HOSTS || "all"
+const ALLOWED_HOSTS = ((process.env.ALLOWED_HOSTS || "all") !== "all" ? process.env.ALLOWED_HOSTS.split(",") : "all")
 const VUE_APP_TALEMATE_BACKEND_WEBSOCKET_URL = process.env.VUE_APP_TALEMATE_BACKEND_WEBSOCKET_URL || null
-
-// if ALLOWED_HOSTS is set and has , then split it
-if (ALLOWED_HOSTS !== "all") {
-  ALLOWED_HOSTS = ALLOWED_HOSTS.split(",")
-}
 
 console.log("ALLOWED_HOSTS", ALLOWED_HOSTS)
 console.log("VUE_APP_TALEMATE_BACKEND_WEBSOCKET_URL", VUE_APP_TALEMATE_BACKEND_WEBSOCKET_URL)

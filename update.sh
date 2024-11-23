4eee53e689bddbcec5afdb462ce8ad766bcd6446
vegu-ai-tools <152010387+vegu-ai-tools@users.noreply.github.com>
2024-11-23 16:09:13 +0200

    use python3

--- update.sh
+++ update.sh
@@ -15,13 +15,13 @@ git pull
 
 # Install dependencies with poetry
 echo "Updating virtual environment..."
-python -m poetry install
+python3 -m poetry install
 
 # Check for CUDA
 if command -v nvcc >/dev/null 2>&1; then
     echo "CUDA found. Installing PyTorch with CUDA support..."
-    python -m pip uninstall torch torchaudio -y
-    python -m pip install torch~=2.4.1 torchaudio~=2.4.1 --index-url https://download.pytorch.org/whl/cu121
+    python3 -m pip uninstall torch torchaudio -y
+    python3 -m pip install torch~=2.4.1 torchaudio~=2.4.1 --index-url https://download.pytorch.org/whl/cu121
 else
     echo "CUDA not found. Keeping PyTorch installation without CUDA support..."
 fi

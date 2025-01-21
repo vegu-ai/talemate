326fc83e052dba639eaee44916f4eda4f6e8fc76
vegu-ai-tools <152010387+vegu-ai-tools@users.noreply.github.com>
2025-01-22 01:25:02 +0200

    fix narration highlighting

--- talemate_frontend/src/components/CharacterMessage.vue
+++ talemate_frontend/src/components/CharacterMessage.vue
@@ -86,7 +86,7 @@ export default {
   methods: {
 
     styleHandlerFromPart(part) {
-      if(part.type === '"' || part.type === "") {
+      if(part.type === '"') {
         return 'character';
       }
       return 'narrator';

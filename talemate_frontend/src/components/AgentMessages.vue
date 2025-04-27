23b5d2a4495b35f1dd0f9760e48961f2520b74da
vegu-ai-tools <152010387+vegu-ai-tools@users.noreply.github.com>
2025-04-27 22:18:51 +0300

    prompt tweaks

--- talemate_frontend/src/components/AgentMessages.vue
+++ talemate_frontend/src/components/AgentMessages.vue
@@ -64,7 +64,7 @@ export default {
         },
         agentLabel: {
             type: String,
-            required: true
+            default: "Agent"
         },
         messageReceiveTime: {
             type: Number,

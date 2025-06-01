9ca33c582f6d553053eb0382c09018236b3a797f
vegu-ai-tools <152010387+vegu-ai-tools@users.noreply.github.com>
2025-06-01 15:38:17 +0300

    fix syntax

--- start-backend.bat
+++ start-backend.bat
@@ -1,1 +1,3 @@
-start cmd /k "cd talemate_env\Scripts && activate && cd ../../ && set TALEMATE_DEBUG=1 && python src\talemate\server\run.py runserver --host 0.0.0.0 --port 5050 --backend-only"
+@echo off
+set TALEMATE_DEBUG=1
+start cmd /k "cd talemate_env\Scripts && activate && cd ../../ && python src\talemate\server\run.py runserver --host 0.0.0.0 --port 5050 --backend-only"

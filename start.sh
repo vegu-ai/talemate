c7c537ba78820c8e3a4eeb181b7cdcd4ec73e96e
Ztripez von Matérn <ztripez@bobby.se>
2025-06-23 01:58:19 +0200

    migrate from poetry to uv package manager

--- start.sh
+++ start.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-. talemate_env/bin/activate
-python src/talemate/server/run.py runserver --host 0.0.0.0 --port 5050
+uv run src/talemate/server/run.py runserver --host 0.0.0.0 --port 5050

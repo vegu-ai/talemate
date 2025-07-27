e0753b6dbd5a278245a7e2d33d1deda1662b5c49
vegu-ai-tools <152010387+vegu-ai-tools@users.noreply.github.com>
2025-07-27 21:02:23 +0300

    install cuda script updated

--- install-cuda.sh
+++ install-cuda.sh
@@ -4,4 +4,4 @@
 uv pip uninstall torch torchaudio
 
 # install torch and torchaudio with CUDA support
-uv pip install torch~=2.7.0 torchaudio~=2.7.0 --index-url https://download.pytorch.org/whl/cu128
+uv pip install torch~=2.7.1 torchaudio~=2.7.1 --index-url https://download.pytorch.org/whl/cu128

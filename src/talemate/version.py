ec059fc6ddaf5b8c4a40863ef86de6dc33c0efaf
vegu-ai-tools <152010387+vegu-ai-tools@users.noreply.github.com>
2026-03-14 11:53:26 +0200

    set dev version

--- src/talemate/version.py
+++ src/talemate/version.py
@@ -1,3 +1,3 @@
 __all__ = ["VERSION"]
 
-VERSION = "0.36.0.dev1"
+VERSION = "0.36.0.dev2"
--- talemate_frontend/src/constants/version.js
+++ talemate_frontend/src/constants/version.js
@@ -1,4 +1,4 @@
-export const FRONTEND_VERSION = "0.36.0.dev1";
+export const FRONTEND_VERSION = "0.36.0.dev2";
 
 export function versionsMatch(backendVersion) {
   return FRONTEND_VERSION === backendVersion;

a10f706d17fcd52f0195ed237600ef71d4be5502
veguAI <152010387+vegu-ai-tools@users.noreply.github.com>
2026-08-08 02:58:21 +0300

    set 0.39.0.dev3

--- talemate_frontend/src/constants/version.js
+++ talemate_frontend/src/constants/version.js
@@ -1,4 +1,4 @@
-export const FRONTEND_VERSION = "0.39.0.dev2";
+export const FRONTEND_VERSION = "0.39.0.dev3";
 
 export function versionsMatch(backendVersion) {
   return FRONTEND_VERSION === backendVersion;
